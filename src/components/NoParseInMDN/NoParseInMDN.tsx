4cc77d7b119d8e3bdf1ae797145d1a4fbe48583f
hiroya-uga <hiroya.uga@gmail.com>
2023-09-28 07:06:24 +0900

    Fix to remove break line in the table

--- src/components/NoParseInMDN/NoParseInMDN.tsx
+++
@@ -1,9 +0,0 @@
-import { HTMLAttributes } from 'react';
-
-type Props = Pick<HTMLAttributes<HTMLHeadingElement>, 'children'>;
-
-export const NoParseInMDN = ({ children }: Props) => {
-  console.log({ children });
-
-  return <>{children}</>;
-};
