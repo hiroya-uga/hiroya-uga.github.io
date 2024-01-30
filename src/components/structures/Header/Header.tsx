b158a69963a8576bd96d59ff5c00f4226b123c65
hiroya-uga <hiroya.uga@gmail.com>
2024-01-31 00:30:42 +0900

    Update Header.tsx

--- src/components/structures/Header/Header.tsx
+++ src/components/structures/Header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="py-8 sm:py-16 px-4 sm:pl-10 mb-4">
+    <header className="py-8 sm:py-16 px-4 sm:pl-10">
       <div className="max-w-content mx-auto">
         <p className="relative -left-4 -top-2">
           <a href="../" className="inline-block py-2 px-4">
