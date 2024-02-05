387f96d9117681ca4996d3d2224e9fe702f15924
hiroya-uga <hiroya.uga@gmail.com>
2024-02-06 04:45:48 +0900

    Update Header.tsx

--- src/components/structures/Header/Header.tsx
+++ src/components/structures/Header/Header.tsx
@@ -7,10 +7,10 @@ export const Header = () => {
         <p className="relative -left-4 -top-2">
           <Link
             href="../"
-            className="group rounded-md inline-block py-2 px-4 no-underline hover:bg-white focus:bg-white transition-colors delay-0 duration-300"
+            className="group rounded-md inline-block py-2 px-4 no-underline sm:hover:bg-white sm:focus:bg-white sm:transition-colors sm:delay-0 sm:duration-300"
           >
             <span className="underline">../</span>
-            <span className="opacity-0 delay-0 duration-300 transition-opacity ml-4 group-focus:opacity-100 group-hover:opacity-100">
+            <span className="sm:opacity-0 sm:delay-0 sm:duration-300 sm:transition-opacity ml-4 group-focus:opacity-100 group-hover:opacity-100">
               １つ上のページへ戻る
             </span>
           </Link>
