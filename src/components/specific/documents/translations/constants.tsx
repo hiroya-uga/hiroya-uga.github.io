40a4e182c574df4b6aa88824f9ef6bb5a6121777
hiroya-uga <hiroya.uga@gmail.com>
2024-02-06 04:20:18 +0900

    Update constants.tsx

--- src/components/specific/documents/translations/constants.tsx
+++ src/components/specific/documents/translations/constants.tsx
@@ -1,7 +1,7 @@
 export const WEB_SPEC_TRANSLATIONS_NOTE = () => {
   return (
     <>
-      一部リンク先を<a href="https://waic.jp/">ウェブアクセシビリティ基盤委員会</a>または
+      一部リンク先をサイト内類似コンテンツ、<a href="https://waic.jp/">ウェブアクセシビリティ基盤委員会</a>または
       <a href="https://github.com/momdo/momdo.github.io/wiki">momdo氏</a>
       の管理する日本語訳に変更しています。
     </>
