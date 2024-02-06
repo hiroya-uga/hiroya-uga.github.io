2994e3b8b7269afe35efd00d6365fe4be4685861
hiroya-uga <hiroya.uga@gmail.com>
2024-02-07 00:57:54 +0900

    Change to use a element intead of Link component

--- src/components/structures/Footer/Footer.tsx
+++ src/components/structures/Footer/Footer.tsx
@@ -10,6 +10,7 @@ export const Footer = () => {
   const isTop = pathname === '/';
 
   return (
+    // Linkコンポーネントを使うと別レイアウト階層に移動した時に layout.css が引き継がれてしまう
     <footer className={clsx([isTop ? 'mt-12 sm:mt-20' : 'mt-48', 'px-[16px]'])}>
       <div className="border-t border-t-gray-400 py-10 text-center max-w-content mx-auto sm:pb-12">
         <p className="text-xs">
