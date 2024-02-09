07cf9d367446494d5949ab7960a635e5bc566b03
hiroya-uga <hiroya.uga@gmail.com>
2024-02-10 03:37:24 +0900

    Add metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
-'use client';
-
 import '@/app/globals.css';
+import { URL_ORIGIN } from '@/constants/meta';
+
+import { Metadata } from 'next';
 
 export default function DocumentLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
+
+export const metadata: Metadata = {
+  metadataBase: new URL(URL_ORIGIN),
+};
