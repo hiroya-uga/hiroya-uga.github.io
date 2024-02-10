0d8295f4e99cbe970eb1b7a6e3bf64a51251a302
hiroya-uga <hiroya.uga@gmail.com>
2024-02-11 05:36:41 +0900

    Update header style

--- src/components/structures/Header/SiteName.tsx
+++ src/components/structures/Header/SiteName.tsx
@@ -12,14 +12,14 @@ export const SiteName = () => {
         </span>
       </p> */}
 
-      <p className="font-mono text-[0.78125rem] mb-4 lg:mb-0 lg:fixed lg:top-0 lg:left-0 lg:overflow-auto lg:h-full">
+      <p className="text-[0.78125rem] mb-4 lg:mb-0 lg:fixed lg:top-0 lg:left-0 lg:overflow-auto lg:h-full">
         <span className="block overflow-auto lg:overflow-visible bg-white lg:w-[2rem] lg:min-w-[2rem] lg:min-h-[18.5rem] lg:h-[29.2929vh] lg:pb-4 lg:box-content">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             role="none presentation"
             className="ml-auto w-[18.5rem] min-w-[18.5rem] h-[1.465rem] lg:w-[2rem] lg:h-[18.5rem] lg:min-w-[2rem] lg:min-h-[18.5rem]"
           >
-            <text x="50%" y="50%" textAnchor="middle" className="translate-y-[4px] lg:hidden">
+            <text x="58%" y="50%" textAnchor="middle" className="translate-y-[4px] lg:hidden">
               {/* bootstrapを読み込んでいる画面だとunderlineがつかない */}
               <a href="/" className="underline">
                 {SITE_NAME}
@@ -28,8 +28,8 @@ export const SiteName = () => {
             </text>
 
             <text
-              x="-900%"
-              y="8%"
+              x="-750%"
+              y="6.5%"
               textAnchor="start"
               style={{
                 // bootstrapを読み込んでいる画面だと .hidden に display: none!imortant がついている
