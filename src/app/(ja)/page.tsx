74dd63b1e890c61b8d21cb0d480fdee6ae0cf4f5
hiroya-uga <hiroya.uga@gmail.com>
2024-03-01 10:02:14 +0900

    Update top

--- src/app/(ja)/page.tsx
+++ src/app/(ja)/page.tsx
@@ -1,8 +1,10 @@
-import { Counter, TopImage } from '@/app/(ja)/Client';
+import { Counter, LinkList, TopImage } from '@/app/(ja)/Client';
 import { Footer } from '@/components/structures/Footer';
 import { SITE_NAME } from '@/constants/meta';
 import { externalMediaLinkList } from '@/data/externalMediaLinkList';
 import { Metadata } from '@/types/seo';
+
+import { useId } from 'react';
 
 import clsx from 'clsx';
 import Image from 'next/image';
@@ -17,36 +19,9 @@ export const metadata: Metadata = {
   },
 };
 
-const LinkList = ({
-  list,
-}: {
-  list: {
-    href: string;
-    title: string;
-  }[];
-}) => {
-  return (
-    <ul className="mb-2 pl-4 leading-normal sm:text-sm sm:grid sm:grid-cols-3 sm:gap-4 sm:pl-0">
-      {list.map(({ title, href }, index, { length }) => {
-        return (
-          <li
-            key={href}
-            className={clsx([
-              index !== length - 1 && 'pb-3 sm:pb-0',
-              'list-disc sm:grid sm:min-h-[calc(1em_+_1em_*_2_*_1.5)]',
-            ])}
-          >
-            <a href={href} className="break-all sm:bg-gray-200 sm:rounded sm:p-2 sm:grid sm:content-center">
-              {title}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
-
 export default function Home() {
+  const captionId = useId();
+
   return (
     <>
       <header className="py-12 px-4 sm:py-16">
@@ -66,7 +41,7 @@ export default function Home() {
 
       <main>
         <div className="mb-4 max-w-content mx-auto">
-          <TopImage />
+          <TopImage captionId={captionId} />
         </div>
 
         <div className="px-[16px]">
@@ -150,7 +125,7 @@ export default function Home() {
                           {/* {item.isWip && <b>（WIP）</b>} */}
                         </span>
                         <span
-                          className="grid bg-gray-200 place-content-center text-5xl aspect-[1.618_/_1] mb-4 rounded-md leading-none font-emoji"
+                          className="grid bg-gray-200 place-content-center text-5xl aspect-[1.618_/_1] mb-3 rounded-md leading-none font-emoji"
                           aria-hidden="true"
                         >
                           {emoji}
@@ -193,60 +168,105 @@ export default function Home() {
             <p className="text-right mb-12">
               <a href="/documents/media/">外部メディアリンク一覧</a>
             </p>
+          </div>
+        </div>
 
-            <h3 className="text-lg mb-4">その他のメニュー</h3>
+        <div className="px-[16px] py-11 pb-12 bg-slate-200 mb-12 sm:mb-28">
+          <div className="max-w-content mx-auto">
+            <h2 className="text-2xl mb-4">カテゴリー</h2>
 
-            <div className="mb-12 sm:mb-28">
-              <LinkList
-                list={[
-                  {
-                    href: '/tools',
-                    title: 'ツール集',
-                  },
-                  {
-                    href: '/documents',
-                    title: '資料集',
-                  },
-                ]}
-              />
-            </div>
+            <ul className="grid grid-cols-2 gap-4 sm:grid-cols-3 sm:gap-8">
+              {[
+                {
+                  emoji: '🔧',
+                  href: '/tools',
+                  title: 'ツール集',
+                },
+                {
+                  emoji: '📚',
+                  href: '/documents',
+                  title: '資料集',
+                },
+              ].map(({ emoji, href, title }) => {
+                return (
+                  <li key={href}>
+                    <a href={href} className="no-underline block">
+                      <span
+                        className="grid bg-gray-50 place-content-center text-5xl aspect-[1.618_/_1] mb-2 rounded-md leading-none font-emoji"
+                        aria-hidden="true"
+                      >
+                        {emoji}
+                      </span>
+                      <span className="font-bold">→ {title}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
 
-            <h2 className="text-2xl mb-4">Bookmarks</h2>
+        <div className="px-[16px]">
+          <div className="max-w-content mx-auto">
+            <h2 className="text-2xl mb-2">Bookmarks</h2>
+
+            <p className="mb-4">外部サイトへのリンク集です。</p>
 
             <LinkList
               list={[
                 {
                   href: 'https://html.spec.whatwg.org/multipage/',
+                  hrefLang: 'en',
                   title: 'HTML Living Standard',
+                  japanese: 'https://momdo.github.io/html/',
                 },
                 {
-                  href: 'https://www.w3.org/WAI/ARIA/apg/',
-                  title: 'ARIA Authoring Practices Guide (APG)',
+                  href: 'https://www.w3.org/TR/wai-aria-1.2/',
+                  hrefLang: 'en',
+                  title: 'Accessible Rich Internet Applications (WAI-ARIA) 1.2',
+                  japanese: 'https://momdo.github.io/wai-aria-1.2/',
                 },
                 {
                   href: 'https://www.w3.org/TR/html-aria/',
+                  hrefLang: 'en',
                   title: 'ARIA in HTML',
+                  japanese: 'https://momdo.github.io/html-aria/',
                 },
                 {
                   href: 'https://w3c.github.io/wcag/guidelines/22/',
+                  hrefLang: 'en',
                   title: 'Web Content Accessibility Guidelines (WCAG) 2.2',
+                  japanese: 'https://waic.jp/translations/WCAG22/',
                 },
                 {
                   href: 'https://www.w3.org/TR/wcag-3.0/',
+                  hrefLang: 'en',
                   title: 'W3C Accessibility Guidelines (WCAG) 3.0',
                 },
                 {
                   href: 'https://www.w3.org/TR/html-aam-1.0/',
+                  hrefLang: 'en',
                   title: 'HTML Accessibility API Mappings 1.0',
+                },
+                {
+                  href: 'https://www.w3.org/WAI/ARIA/apg/',
+                  hrefLang: 'en',
+                  title: 'ARIA Authoring Practices Guide (APG)',
+                },
+                {
+                  href: 'https://www.w3.org/Provider/Style/URI.html',
+                  hrefLang: 'en',
+                  title: "Hypertext Style: Cool URIs don't change.",
+                  japanese: 'https://www.kanzaki.com/docs/Style/URI.html',
                 },
               ]}
             />
 
-            <div className="mt-12 pt-12 sm:mt-28 sm:pt-28 border-t border-t-gray-400 border-dashed mb-8 grid-cols-[1fr_auto] grid-rows-[auto_1fr] sm:grid sm:gap-x-16 sm:mb-0">
+            <div className="mt-12 pt-12 sm:mt-28 sm:pt-28 border-t border-t-gray-400 border-dashed mb-8 grid-cols-[1fr_auto] grid-rows-[auto_1fr] sm:grid sm:gap-x-12 sm:mb-0">
               <h2 className="text-2xl mt-0 mb-4 row-start-1 row-end-2 col-start-1 col-end-2">PROFILE</h2>
 
-              <p className="leading-none mb-8 flex items-center gap-4 justify-end row-start-1 row-end-3 col-start-2 col-end-3 sm:gap-x-6 sm:m-0">
-                <span className="whitespace-nowrap sm:grow text-justify text-xl">
+              <p className="leading-none mb-8 flex mx-auto pr-4 items-center gap-4 sm:row-start-1 sm:row-end-3 sm:col-start-2 sm:col-end-3 sm:gap-x-6 sm:m-0 max-w-[22.5rem] aspect-[8_/_5] sm:aspect-auto">
+                <span className="whitespace-nowrap grow text-center text-xl leading-tight">
                   <span className="tracking-widest">宇賀景哉</span>
                   <span
                     className="block text-xs palt"
@@ -257,15 +277,15 @@ export default function Home() {
                     Hiroya UGA
                   </span>
                 </span>
-                <span className="w-40 min-w-[160px]">
-                  <Image width={160} height={160} src="/favicon.png" alt="" />
+                <span className="w-40">
+                  <Image width={160} height={160} src="/profile.png" alt="" />
                 </span>
               </p>
 
               <div className="row-start-2 row-end-3 col-start-1 col-end-2">
-                <div className="sm:max-w-xl sm:mb-4 sm:pr-4 palt">
-                  <p>大器晩成型のフロントエンドWeb開発者。</p>
-                  <p className="inline-block">
+                <div className="sm:max-w-xl sm:mb-4 sm:pr-4">
+                  <p>大器晩成型のフロントエンドWeb開発者。Web標準の話が好き。</p>
+                  <p className="inline-block sm:palt">
                     「元気に楽しく、自信と情熱を持って、すべてのWebをアクセシブルに」がモットー。
                   </p>
                   <p>アイコンは昔の上司がスマホで描いて送ってくれた似顔絵。</p>
