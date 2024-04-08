c56d48f7e3ffdfbfb1547dd54c0735fa4f8bffe8
hiroya-uga <hiroya.uga@gmail.com>
2024-04-08 16:07:32 +0900

    Update character-count

--- src/app/(ja)/(common)/tools/character-count/Client.tsx
+++ src/app/(ja)/(common)/tools/character-count/Client.tsx
@@ -20,12 +20,10 @@ const countCharacters = ({
   isHalfWidthCount?: boolean;
 }) => {
   const halfWidthLength = isHalfWidthCount ? countHalfWidthCharacters(value) / 2 : 0;
+  const segmenter = new Intl.Segmenter('ja-JP', { granularity: 'grapheme' });
+  const string = isIgnoreWhitespace ? value.replace(/\s/g, '') : value.replace(/\n/g, '');
 
-  if (isIgnoreWhitespace) {
-    return value.replace(/\s/g, '').length - halfWidthLength;
-  }
-
-  return value.replace(/\n/g, '').length - halfWidthLength;
+  return [...segmenter.segment(string)].length - halfWidthLength;
 };
 
 const countLines = ({ value, isIgnoreEmptyLines }: { value: string; isIgnoreEmptyLines?: boolean }) => {
@@ -43,7 +41,7 @@ const countBytes = ({ value }: { value: string }) => {
 };
 
 const countPages = ({ value }: { value: string }) => {
-  const charCount = countCharacters({ value, isIgnoreWhitespace: true });
+  const charCount = countCharacters({ value });
   return Math.ceil(charCount / 400);
 };
 
@@ -95,59 +93,6 @@ export const CharacterCountContent = ({ id }: { id: string }) => {
 
   return (
     <div className={clsx(['transition-[opacity_visibility]', isFirstView ? 'invisible opacity-0' : ''])}>
-      <div className="mb-4">
-        <Details summary="設定" id={`${id}-details`}>
-          <div className="p-4">
-            <p className="mb-4 text-right">
-              <label>
-                文字サイズ
-                <input
-                  type="number"
-                  value={fontSize}
-                  className="mx-2 w-12 border border-gray-400 px-2 text-right"
-                  onChange={({ currentTarget }) => {
-                    const currentValue = currentTarget.value;
-
-                    if (isNaN(Number(currentValue))) {
-                      setFontSize(DEFAULT_FONT_SIZE);
-                      return;
-                    }
-
-                    setFontSize(currentValue);
-                  }}
-                  onBlur={({ currentTarget }) => {
-                    const currentValue = currentTarget.value;
-
-                    if (currentValue === '') {
-                      setFontSize(DEFAULT_FONT_SIZE);
-                    }
-                  }}
-                />
-                px
-              </label>
-            </p>
-
-            <p className="mb-4 text-right">
-              <label className="ml-auto grid w-fit grid-cols-[auto_1fr] gap-2">
-                <span className="grow">リアルタイムカウント</span>
-                <span>
-                  <Switch checked={isAutoCount} dispatch={setIsAutoCount} />
-                </span>
-              </label>
-            </p>
-
-            <p className="text-right">
-              <label className="ml-auto grid w-fit grid-cols-[auto_1fr] gap-2">
-                <span className="grow">半角文字は２文字で１文字としてカウントする</span>
-                <span>
-                  <Switch checked={isHalfWidthCount} dispatch={setIsHalfWidthCount} />
-                </span>
-              </label>
-            </p>
-          </div>
-        </Details>
-      </div>
-
       <h2>
         <label htmlFor={id}>本文</label>
       </h2>
@@ -193,7 +138,7 @@ export const CharacterCountContent = ({ id }: { id: string }) => {
             isAutoCount && 'invisible opacity-0',
           ])}
         >
-          字数を数える
+          文字数を数える
         </button>
         <button
           type="button"
@@ -207,56 +152,110 @@ export const CharacterCountContent = ({ id }: { id: string }) => {
         </button>
       </p>
 
-      <h2>結果</h2>
-      <div className="grid w-fit grid-cols-2 gap-x-4 gap-y-2">
-        <p className="col-span-full grid grid-cols-subgrid">
-          <label className="grow" htmlFor={`${id}-文字数`}>
-            文字数
-          </label>
-          <output id={`${id}-文字数`} className="border border-gray-400 bg-white px-2">
-            {countCharacters({ value, isHalfWidthCount })}
-          </output>
-        </p>
-        <p className="col-span-full grid grid-cols-subgrid">
-          <label className="grow" htmlFor={`${id}-文字数（空白文字を除く）`}>
-            文字数（空白文字を除く）
-          </label>
-          <output id={`${id}-文字数（空白文字を除く）`} className="border border-gray-400 bg-white px-2">
-            {countCharacters({ value, isIgnoreWhitespace: true, isHalfWidthCount })}
-          </output>
-        </p>
-        <p className="col-span-full grid grid-cols-subgrid">
-          <label className="grow" htmlFor={`${id}-行数`}>
-            行数
-          </label>
-          <output id={`${id}-行数`} className="border border-gray-400 bg-white px-2">
-            {countLines({ value })}
-          </output>
-        </p>
-        <p className="col-span-full grid grid-cols-subgrid">
-          <label className="grow" htmlFor={`${id}-行数（空行を除く）`}>
-            行数（空行を除く）
-          </label>
-          <output id={`${id}-行数（空行を除く）`} className="border border-gray-400 bg-white px-2">
-            {countLines({ value, isIgnoreEmptyLines: true })}
-          </output>
-        </p>
-        <p className="col-span-full grid grid-cols-subgrid">
-          <span className="grow">
-            <label htmlFor={`${id}-原稿用紙換算（400文字）`}>原稿用紙換算（400文字）</label>×
-          </span>
-          <output id={`${id}-原稿用紙換算（400文字）`} className="border border-gray-400 bg-white px-2">
-            {countPages({ value })}枚
-          </output>
-        </p>
-        <p className="col-span-full grid grid-cols-subgrid">
-          <label className="grow" htmlFor={`${id}-バイト数（UTF`}>
-            バイト数（UTF-8）
-          </label>
-          <output id={`${id}-バイト数（UTF`} className="border border-gray-400 bg-white px-2">
-            {countBytes({ value })}
-          </output>
-        </p>
+      <div className="mt-24 grid-cols-[60%_40%] text-sm sm:grid lg:text-base">
+        <div className="mb-24 sm:mb-0 sm:pr-14">
+          <h2 className="mt-0">結果</h2>
+          <div className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-2 md:w-fit md:grid-cols-2">
+            <p className="col-span-full grid grid-cols-subgrid">
+              <label className="grow" htmlFor={`${id}-文字数`}>
+                文字数
+              </label>
+              <output id={`${id}-文字数`} className="border border-gray-400 bg-white px-2 text-base">
+                {countCharacters({ value, isHalfWidthCount })}
+              </output>
+            </p>
+            <p className="col-span-full grid grid-cols-subgrid">
+              <label className="grow" htmlFor={`${id}-文字数（空白文字を除く）`}>
+                文字数（空白文字を除く）
+              </label>
+              <output id={`${id}-文字数（空白文字を除く）`} className="border border-gray-400 bg-white px-2 text-base">
+                {countCharacters({ value, isIgnoreWhitespace: true, isHalfWidthCount })}
+              </output>
+            </p>
+            <p className="col-span-full grid grid-cols-subgrid">
+              <label className="grow" htmlFor={`${id}-行数`}>
+                行数
+              </label>
+              <output id={`${id}-行数`} className="border border-gray-400 bg-white px-2 text-base">
+                {countLines({ value })}
+              </output>
+            </p>
+            <p className="col-span-full grid grid-cols-subgrid">
+              <label className="grow" htmlFor={`${id}-行数（空行を除く）`}>
+                行数（空行を除く）
+              </label>
+              <output id={`${id}-行数（空行を除く）`} className="border border-gray-400 bg-white px-2 text-base">
+                {countLines({ value, isIgnoreEmptyLines: true })}
+              </output>
+            </p>
+            <p className="col-span-full grid grid-cols-subgrid">
+              <span className="grow">
+                <label htmlFor={`${id}-原稿用紙換算（400文字）`}>原稿用紙換算（400文字）</label>×
+              </span>
+              <output id={`${id}-原稿用紙換算（400文字）`} className="border border-gray-400 bg-white px-2 text-base">
+                {countPages({ value })}枚
+              </output>
+            </p>
+            <p className="col-span-full grid grid-cols-subgrid">
+              <label className="grow" htmlFor={`${id}-バイト数（UTF`}>
+                バイト数（UTF-8）
+              </label>
+              <output id={`${id}-バイト数（UTF`} className="border border-gray-400 bg-white px-2 text-base">
+                {countBytes({ value })}
+              </output>
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <h2 className="mt-0">設定</h2>
+          <p className="mb-4 text-right">
+            <label>
+              文字サイズ
+              <input
+                type="number"
+                value={fontSize}
+                className="mx-2 w-12 border border-gray-400 px-2 text-right"
+                onChange={({ currentTarget }) => {
+                  const currentValue = currentTarget.value;
+
+                  if (isNaN(Number(currentValue))) {
+                    setFontSize(DEFAULT_FONT_SIZE);
+                    return;
+                  }
+
+                  setFontSize(currentValue);
+                }}
+                onBlur={({ currentTarget }) => {
+                  const currentValue = currentTarget.value;
+
+                  if (currentValue === '') {
+                    setFontSize(DEFAULT_FONT_SIZE);
+                  }
+                }}
+              />
+              px
+            </label>
+          </p>
+
+          <p className="mb-4 text-right">
+            <label className="flex items-center gap-2">
+              <span className="grow">リアルタイムカウント</span>
+              <span>
+                <Switch checked={isAutoCount} dispatch={setIsAutoCount} />
+              </span>
+            </label>
+          </p>
+
+          <p className="text-right">
+            <label className="flex items-center gap-2">
+              <span className="grow">半角文字は２文字で１文字としてカウント</span>
+              <span>
+                <Switch checked={isHalfWidthCount} dispatch={setIsHalfWidthCount} />
+              </span>
+            </label>
+          </p>
+        </div>
       </div>
     </div>
   );
