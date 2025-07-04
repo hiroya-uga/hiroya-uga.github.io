908667793aa757680d1d7698295d1c9b5edc6e4b
hiroya-uga <hiroya.uga@gmail.com>
2025-07-04 11:32:09 +0900

    Update page.tsx

--- src/app/(ja)/(articles)/articles/[...slug]/page.tsx
+++ src/app/(ja)/(articles)/articles/[...slug]/page.tsx
@@ -170,7 +170,7 @@ export default async function PostPage({ params }: { params: Promise<{ slug: str
       <main>
         <ArticleMain post={post} />
         <div className="mx-content-inline @container not-empty:mt-30 text-center">
-          <p className="mb-12">
+          <p className="mb-paragraph">
             <TweetLink
               message={`【${categoryName}】${post.meta.title.replaceAll('\n', '')}`}
               className="not-[[href]]:opacity-0 not-[[href]]:invisible mx-auto block w-fit rounded-lg border border-solid bg-black px-4 text-white no-underline transition-[opacity,visibility,box-shadow] hover:underline hover:shadow-lg"
