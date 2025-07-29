f1ac6d470b7a64bc4f5ef2b44ea838291fee8546
hiroya-uga <hiroya.uga@gmail.com>
2025-07-29 22:35:50 +0900

    Update render-text-in-react

--- src/app/(ja)/(common)/tools/render-text-in-react/page.tsx
+++ src/app/(ja)/(common)/tools/render-text-in-react/page.tsx
@@ -2,6 +2,7 @@ import { RenderTextInReactPlaygroundContent } from '@/app/(ja)/(common)/tools/re
 import { TweetLink } from '@/components/Clickable/TweetLink';
 import { PageTitle } from '@/components/structures/PageTitle';
 import { getMetadata } from '@/utils/get-metadata';
+import Link from 'next/link';
 
 export const metadata = getMetadata('/tools/render-text-in-react');
 
@@ -9,7 +10,13 @@ export default function Page() {
   return (
     <>
       <PageTitle {...metadata}>
-        <p>検証環境：React 19 / macOS 15 + VO / iOS 18 Safari + VO</p>
+        <p>iOS 18 Safari + VoiceOverで読み上げが変わる様子が確認できます。</p>
+        <p>
+          参考記事：
+          <Link href="/articles/tech-blog/2025/07-27-react-text-node/">
+            Reactのテキストは記述の仕方で読み上げが変わってしまう件について
+          </Link>
+        </p>
       </PageTitle>
 
       <RenderTextInReactPlaygroundContent />
