e6d0a8d889b67dd92a49d45e856f55836f04d3a0
hiroya-uga <hiroya.uga@gmail.com>
2025-08-02 01:48:44 +0900

    Change to include external articles

--- src/app/(ja)/(common)/articles/page.tsx
+++ src/app/(ja)/(common)/articles/page.tsx
@@ -2,6 +2,7 @@ import { ArticleList } from '@/components/List';
 import { PageTitle } from '@/components/structures/PageTitle';
 import { getMetadata } from '@/utils/get-metadata';
 import { getAllArticles } from '@/utils/ssg-articles';
+import Link from 'next/link';
 
 export const metadata = getMetadata('/articles');
 
@@ -11,7 +12,10 @@ export default async function Page() {
   return (
     <>
       <PageTitle title={metadata.pageTitle} description={metadata.description}>
-        <p>不定期更新＆工事中。まったりUI調整しています🍵</p>
+        <p className="mb-paragraph">不定期更新＆工事中。まったりUI調整しています🍵</p>
+        <p>
+          <Link href="/documents/media/">外部メディアリンク一覧はこちら</Link>をご覧ください。
+        </p>
       </PageTitle>
       <ArticleList type="thumbnail" list={blogs} />
     </>
--- src/app/(ja)/parts/ArticleListForTop.tsx
+++ src/app/(ja)/parts/ArticleListForTop.tsx
@@ -1,9 +1,29 @@
+import { externalMediaLinkList } from '@/data/external-media-link-list';
 import { resolveCategoryName } from '@/utils/articles';
 import { getAllArticles } from '@/utils/ssg-articles';
 import Link from 'next/link';
 
 export const ArticleListForTop = async () => {
-  const blogs = await getAllArticles();
+  const blogs = [
+    ...(await getAllArticles()),
+    ...externalMediaLinkList
+      .filter((item) => item.type === 'article')
+      .map((item) => ({
+        title: item.title,
+        pathname: item.href,
+        publishedAt: item.date,
+      })),
+  ].sort((a, b) => {
+    const aDate = new Date(a.publishedAt);
+    const bDate = new Date(b.publishedAt);
+    if (aDate.getTime() < bDate.getTime()) {
+      return 1;
+    }
+    if (aDate.getTime() > bDate.getTime()) {
+      return -1;
+    }
+    return 0;
+  });
 
   return (
     <>
@@ -22,7 +42,7 @@ export const ArticleListForTop = async () => {
                   {article.publishedAt.replace(/-/g, '/')}
                 </time>
                 <span className="@w500:col-start-2 text-center font-mono text-sm">
-                  {resolveCategoryName(article.pathname.split('/')[2])}
+                  {resolveCategoryName(article.pathname)}
                 </span>
                 <span className="@w500:col-start-3 grid w-full justify-start">
                   <Link href={article.pathname} title={title} className="truncate">
