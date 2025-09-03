aa0438c9baca440249326eae4ee24d064fcbf413
hitoya-uga <hiroya.uga@gmail.com>
2025-09-04 01:06:16 +0900

    Support Youtube player

--- src/components/Icons/LoadingIcon.tsx
+++ src/components/Icons/LoadingIcon.tsx
@@ -7,3 +7,5 @@ export const LoadingIcon = ({ alt = '読み込み中' }: { alt?: string }) => {
     />
   );
 };
+
+export const LOADING_ICON_HTML = `<span class="relative block size-9 origin-center cursor-progress before:absolute before:inset-0 before:size-full before:animate-spin before:rounded-full before:border-2 before:border-gray-300 before:border-t-gray-800 before:content-['']" aria-hidden="true"></span>`;
