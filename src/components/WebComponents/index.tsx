cd1dcf46d8c0d082b96aa8ed77889ea5f839e1aa
hiroya-uga <hiroya.uga@gmail.com>
2025-09-21 21:50:56 +0900

    Update index.tsx

--- src/components/WebComponents/index.tsx
+++ src/components/WebComponents/index.tsx
@@ -1,9 +1,14 @@
 'use client';
 import { LazyImageLoad } from '@/components/WebComponents/lazy-image';
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 
 export const LoadWebComponents = () => {
+  const isInitialized = useRef(false);
+
   useLayoutEffect(() => {
+    if (isInitialized.current) return;
+    isInitialized.current = true;
+
     if (typeof customElements.get('lazy-image') === 'undefined') {
       LazyImageLoad();
     }
