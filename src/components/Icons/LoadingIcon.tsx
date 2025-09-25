f4fd4c9d687c7a90a3c19078f4c9101b48cd0e91
hiroya-uga <hiroya.uga@gmail.com>
2025-09-25 13:07:19 +0900

    Fix style

--- src/components/Icons/LoadingIcon.tsx
+++ src/components/Icons/LoadingIcon.tsx
@@ -1,11 +1,12 @@
+// Safariでレンダリングがおかしくなるので、beforeの代わりにspan要素を使う
 export const LoadingIcon = ({ alt = '読み込み中' }: { alt?: string }) => {
   return (
-    <span
-      role="img"
-      aria-label={alt}
-      className="relative block size-9 origin-center cursor-progress before:absolute before:inset-0 before:size-full before:animate-spin before:rounded-full before:border-2 before:border-gray-300 before:border-t-gray-800 before:content-['']"
-    />
+    <span role="img" aria-label={alt} className="relative block size-9 origin-center cursor-progress">
+      <span className="absolute inset-0 size-full animate-spin rounded-full border-2 border-gray-300 border-t-gray-800"></span>
+    </span>
   );
 };
 
-export const LOADING_ICON_HTML = `<span class="relative block size-9 origin-center cursor-progress before:absolute before:inset-0 before:size-full before:animate-spin before:rounded-full before:border-2 before:border-gray-300 before:border-t-gray-800 before:content-['']" aria-hidden="true"></span>`;
+export const LOADING_ICON_HTML = `<span class="relative block size-9 origin-center cursor-progress" aria-hidden="true">
+  <span class="absolute inset-0 size-full animate-spin rounded-full border-2 border-gray-300 border-t-gray-800"></span>
+</span>`;
