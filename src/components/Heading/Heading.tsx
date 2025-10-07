8f44a5a2b9a315e41b1badfc07a0027c6529b63d
hiroya-uga <hiroya.uga@gmail.com>
2025-10-07 21:02:35 +0900

    Update about pagePlease enter the commit message for your changes. Lines starting

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -8,13 +8,13 @@ type Props = {
 };
 
 export const Heading = ({ level = 2, keepUseMarginTop = false, ...props }: Props) => {
-  const TagName = `h${level}` as 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  const TagName = `h${level}` as const;
 
   return (
     <TagName
       className={clsx([
         'mb-paragraph font-bold leading-snug',
-        keepUseMarginTop === true ? 'mt-[2.5lh]' : 'not-first:mt-[2.5lh]',
+        keepUseMarginTop === true ? 'mt-(--margin-heading-top)' : 'not-first:mt-(--margin-heading-top)',
         level === 2 && '@w640:text-2xl text-xl',
         level === 3 && '@w640:text-xl text-lg',
         level === 4 && '@w640:text-lg text-base',
