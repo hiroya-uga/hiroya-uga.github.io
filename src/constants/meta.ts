4ac922e5e7988ef2d7daa8a74cbc23da09405669
hiroya-uga <hiroya.uga@gmail.com>
2026-03-03 02:57:38 +0900

    Update ArticleJsonLD.tsx

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,7 +1,7 @@
 export const DOMAIN_NAME = process.env.DOMAIN || 'uga.dev';
 export const URL_ORIGIN = process.env.URL_ORIGIN || `https://${DOMAIN_NAME}`;
 export const SITE_NAME = 'uga.dev';
-export const SITE_AUTHOR = 'Hiroya Uga';
+export const SITE_AUTHOR = 'Hiroya UGA';
 export const SITE_SUBTITLE = "A Front-end Engineer's shed";
 export const SITE_DESCRIPTION =
   'Web標準とWebアクセシビリティの話が好きな、大器晩成型のフロントエンドエンジニアの物置。';
