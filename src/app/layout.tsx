c69d9aac991fd33d0bd452daaa8f2004476a125b
hiroya-uga <hiroya.uga@gmail.com>
2026-03-12 20:45:57 +0900

    Add meta[name="text-scale"]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="ja" data-theme={theme} suppressHydrationWarning>
       <head>
+        <meta name="text-scale" content="scale" />
         <link rel="shortcut icon" type="image/png" href="/favicon.png" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
