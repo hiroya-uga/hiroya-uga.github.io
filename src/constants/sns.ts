c51751b28a2e1192b4bb0a9b73e88cb9b73a1bea
hiroya-uga <hiroya.uga@gmail.com>
2026-03-17 23:12:58 +0900

    Move logo images

--- src/constants/sns.ts
+++ src/constants/sns.ts
@@ -1,28 +1,28 @@
 export const SNS_LINKS = [
   {
     href: 'https://github.com/hiroya-uga',
-    src: '/common/images/github.svg',
+    src: '/common/images/logos/github.svg',
     alt: 'GitHub',
     width: 28,
     height: 28,
   },
   {
     href: 'https://x.com/hiroya_UGA',
-    src: '/common/images/twitter.svg',
+    src: '/common/images/logos/twitter.svg',
     alt: 'Twitter',
     width: 32,
     height: 26,
   },
   {
     href: 'https://www.instagram.com/hiroya.uga',
-    src: '/common/images/instagram.svg',
+    src: '/common/images/logos/instagram.svg',
     alt: 'Instagram',
     width: 28,
     height: 28,
   },
   {
     href: 'https://codepen.io/hiroya_uga/',
-    src: '/common/images/codepen.png',
+    src: '/common/images/logos/codepen.png',
     alt: 'Codepen',
     width: 28,
     height: 28,
