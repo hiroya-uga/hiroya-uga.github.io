851aba9747aa9e2dbb7e5b86ac4c9f23ce58530c
hiroya-uga <hiroya.uga@gmail.com>
2026-03-26 08:48:57 +0900

    Move Analytics/Jokes to Layout dir, add AnchorLinkSupport component

---
+++ src/components/Layout/AnchorLinkSupport/AnchorLinkSupport.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export const AnchorLinkSupport = () => {
+  useEffect(() => {
+    const handleHashChange = () => {
+      const { hash } = window.location;
+      if (hash) {
+        const element = document.getElementById(hash.substring(1));
+        if (element) {
+          element.focus({
+            preventScroll: true,
+          });
+        }
+      }
+    };
+
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
+
+  return null;
+};
