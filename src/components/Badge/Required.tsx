83d59cd555a5c115c540f80bb1e1bb85c58d05d5
hiroya-uga <hiroya.uga@gmail.com>
2026-04-02 00:36:30 +0900

    Update style

--- src/components/Badge/Required.tsx
+++ src/components/Badge/Required.tsx
@@ -1,6 +1,6 @@
 export const Required = () => {
   return (
-    <strong className="bg-(--v-color-accent) text-high-contrast-reverse ml-1 inline-block w-fit rounded px-1 py-0.5 align-bottom text-xs font-normal leading-snug">
+    <strong className="bg-required ml-1 inline-block w-fit rounded px-1 py-0.5 align-bottom text-xs font-normal leading-snug text-white">
       必須
     </strong>
   );
