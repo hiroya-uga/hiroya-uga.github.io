b15153d13b0bd1e0094f4e0591e27bc66352438b
hiroya-uga <hiroya.uga@gmail.com>
2026-04-08 09:22:35 +0900

    Fix inline display class for welcome message spans

--- src/components/Pages/HomePage/client/WelcomeMessage/WelcomeMessage.tsx
+++ src/components/Pages/HomePage/client/WelcomeMessage/WelcomeMessage.tsx
@@ -146,13 +146,13 @@ export const WelcomeMessage = () => {
             status === 'loading' ? 'opacity-0' : 'opacity-100',
           ])}
         >
-          <span className="relative inline-block">
+          <span className="relative">
             <span className="sr-only inset-0">{`ようこそ ${SITE_NAME} へ。`}</span>
             <span ref={message1Ref} aria-hidden="true">
               {message[0].mapping.map(([a, b]) => (status === 'already' ? b : a))}
             </span>
           </span>
-          <span className="@w640:inline-block relative block">
+          <span className="@w640:inline relative block">
             <span className="sr-only inset-0">{`あなたは${srOnlyAccessCount}番目の訪問者かもしれません。`}</span>
 
             <span ref={message2Ref} aria-hidden="true">
