7f22ada624cb22e1b15c78a3df4d759f43864556
hiroya-uga <hiroya.uga@gmail.com>
2026-06-03 00:29:34 +0900

    Move browser-info tool to general tools list

--- src/constants/link-list.ts
+++ src/constants/link-list.ts
@@ -81,6 +81,7 @@ export const TOOLS_LINK_LIST: Record<string, ToolPage[]> = {
       pathname: '/tools/an-alt-decision-tree',
       userType: ['developer', 'director', 'planner', 'writer'],
     },
+    { emoji: '🩺', pathname: '/tools/browser-info' },
     {
       emoji: '🧮',
       pathname: '/tools/character-count',
@@ -138,7 +139,6 @@ export const TOOLS_LINK_LIST: Record<string, ToolPage[]> = {
     { emoji: '', pathname: '/tools/accessible-name-and-description-computation' },
     { emoji: '📏', pathname: '/tools/css-units' },
     { emoji: '', pathname: '/tools/dom-events-watcher' },
-    { emoji: '🩺', pathname: '/tools/browser-info' },
     { emoji: '👆', pathname: '/tools/touch-event-touches' },
     { emoji: '⌨', pathname: '/tools/keyboard-event' },
     { emoji: '♾️', pathname: '/tools/kaprekar-number' },
