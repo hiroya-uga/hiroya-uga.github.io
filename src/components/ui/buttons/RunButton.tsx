1a3e3e2b1b3d495753eab403d42584a58e1612fa
hiroya-uga <hiroya.uga@gmail.com>
2026-06-03 00:52:12 +0900

    Make btn-run disabled state cover both :disabled and aria-disabled
    - Apply opacity-50 and skip hover styles when either :disabled or
      aria-disabled="true" matches
    - RunButton now sets aria-disabled instead of native disabled and
      short-circuits onClick/onTouchStart when disabled

--- src/components/ui/buttons/RunButton.tsx
+++ src/components/ui/buttons/RunButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 } & Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'type' | 'onClick' | 'disabled' | 'onTouchStart'>;
 
 const RunButtonComponent = (
-  { type = 'button', children, beforeIcon, afterIcon, ...props }: Readonly<Props>,
+  { type = 'button', children, beforeIcon, afterIcon, disabled, ...props }: Readonly<Props>,
   ref: Ref<HTMLButtonElement>,
 ) => {
   return (
@@ -17,39 +17,46 @@ const RunButtonComponent = (
       {...props}
       type={type}
       ref={ref}
-      className={clsx([
-        'btn-run',
-        'max-w-260px block py-2',
-        beforeIcon && !afterIcon && 'pl-14PX pr-16PX',
-        afterIcon && !beforeIcon && 'pl-16PX pr-14PX',
-        !beforeIcon && !afterIcon && 'px-32PX',
-        beforeIcon && afterIcon && 'px-16PX',
-      ])}
+      aria-disabled={disabled}
+      onClick={disabled ? undefined : props.onClick}
+      onTouchStart={disabled ? undefined : props.onTouchStart}
+      className="btn-run max-w-260px block"
     >
-      {beforeIcon || afterIcon ? (
-        <span
-          className={clsx([
-            'mx-auto grid w-fit place-items-center gap-2',
-            afterIcon && beforeIcon && 'grid-cols-[1rem_auto_1rem]',
-            afterIcon && !beforeIcon && 'grid-cols-[auto_1rem]',
-            !afterIcon && beforeIcon && 'grid-cols-[1rem_auto]',
-          ])}
-        >
-          {beforeIcon && (
-            <span className="relative size-4">
-              <SvgIcon alt="" name={beforeIcon} />
-            </span>
-          )}
-          <span>{children}</span>
-          {afterIcon && (
-            <span className="relative size-4">
-              <SvgIcon alt="" name={afterIcon} />
-            </span>
-          )}
-        </span>
-      ) : (
-        children
-      )}
+      <span
+        className={clsx([
+          'block py-2',
+          disabled && 'opacity-50',
+          beforeIcon && !afterIcon && 'pl-14PX pr-16PX',
+          afterIcon && !beforeIcon && 'pl-16PX pr-14PX',
+          !beforeIcon && !afterIcon && 'px-10PX',
+          beforeIcon && afterIcon && 'px-16PX',
+        ])}
+      >
+        {beforeIcon || afterIcon ? (
+          <span
+            className={clsx([
+              'mx-auto grid w-fit place-items-center gap-2',
+              afterIcon && beforeIcon && 'grid-cols-[1rem_auto_1rem]',
+              afterIcon && !beforeIcon && 'grid-cols-[auto_1rem]',
+              !afterIcon && beforeIcon && 'grid-cols-[1rem_auto]',
+            ])}
+          >
+            {beforeIcon && (
+              <span className="relative size-4">
+                <SvgIcon alt="" name={beforeIcon} />
+              </span>
+            )}
+            <span>{children}</span>
+            {afterIcon && (
+              <span className="relative size-4">
+                <SvgIcon alt="" name={afterIcon} />
+              </span>
+            )}
+          </span>
+        ) : (
+          children
+        )}
+      </span>
     </button>
   );
 };
